dade989e89f7d4cbfb1617cb8263d3abb8d57c50
Mihai <m.t.oconnell@hotmail.com>
2024-07-10 11:03:41 +0200

    Add files via upload

---
+++ WysiwygDemo/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,25 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using WebViewControl;
+
+namespace WysiwygDemo.ViewModels;
+
+public partial class MainWindowViewModel : ViewModelBase
+{
+    private WebView _webview;
+
+    [ObservableProperty]
+    private string _CurrentAddress;
+
+    [RelayCommand]
+    private void ShowDevTools()
+    {
+        _webview.ShowDeveloperTools();
+    }
+
+    public MainWindowViewModel(WebView webView)
+    {
+        _webview = webView;
+        CurrentAddress = "Path\\To\\HTML\\File\\MyPage.html";
+    }
+}
---
+++ WysiwygDemo/Views/MainWindow.axaml.cs
@@ -0,0 +1,18 @@
+using Avalonia.Controls;
+using WebViewControl;
+using WysiwygDemo.ViewModels;
+
+namespace WysiwygDemo.Views
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            WebView.Settings.OsrEnabled = false;
+            WebView.Settings.LogFile = "ceflog.txt";
+
+            InitializeComponent();
+            DataContext = new MainWindowViewModel(this.FindControl<WebView>("webview"));
+        }
+    }
+}
